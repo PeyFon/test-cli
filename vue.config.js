c455b04423d46c873106b33f1d4cdc1ac5352e9c
PeyFon <60815493+PeyFon@users.noreply.github.com>
2022-06-05 17:27:16 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -22,7 +22,7 @@ module.exports = defineConfig({
       filename: "index.html",
       // 当使用 title 选项时，
       // template 中的 title 标签需要是 <title><%= htmlWebpackPlugin.options.title %></title>
-      title: "前端模板项目",
+      title: "测试",
       // 在这个页面中包含的块，默认情况下会包含
       // 提取出来的通用 chunk 和 vendor chunk。
       chunks: ["chunk-vendors", "chunk-common", "index"]
@@ -36,7 +36,7 @@ module.exports = defineConfig({
     //   filename: "system.html",
     //   // 当使用 title 选项时，
     //   // template 中的 title 标签需要是 <title><%= htmlWebpackPlugin.options.title %></title>
-    //   title: "前端模板项目后台",
+    //   title: "测试1",
     //   // 在这个页面中包含的块，默认情况下会包含
     //   // 提取出来的通用 chunk 和 vendor chunk。
     //   chunks: ["chunk-vendors", "chunk-common", "system"]
